67ac7a952a2a57c4e316a2fa1e8db29a9ef064d4
gerard@bci.pl <ALamakota123!>
2019-12-19 20:31:51 +0100

    Dodana możliwość wygenerowania n-tablic.

--- App.java
+++ App.java
@@ -12,31 +12,40 @@ public class App {
     public static void main(String[] args) {
         int tableSize;
         int highestNumberAbs;
+        int numberOfArrays;
 
         if (args.length != 0) {
             tableSize = Integer.parseInt(args[0]);
             highestNumberAbs = Integer.parseInt(args[1]);
+            numberOfArrays = Integer.parseInt(args[2]);
+
         } else {
-            Scanner scanner= new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Podaj ilość tablic:");
+            numberOfArrays = scanner.nextInt();
+
             System.out.print("Podaj rozmiar tablicy:");
             tableSize = scanner.nextInt();
             System.out.print("Podaj najwyższą liczbę:");
             highestNumberAbs = scanner.nextInt();
+
             System.out.print("");
         }
 
+        for (int i = 0; i < numberOfArrays; i++) {
 
-        long time0 = nanoTime();
+            long time0 = nanoTime();
 
-        int[] tableOryginal = TableGeneratorInt.tableGeneratorInt(tableSize, highestNumberAbs);
-        int[] tableSortedDesc = TableSorting.tableSortingIntDesc(tableOryginal);
-        int[] tableSortedAsc = TableSorting.tableSortingIntAsc(tableOryginal);
+            int[] tableOryginal = TableGeneratorInt.tableGeneratorInt(tableSize, highestNumberAbs);
+            int[] tableSortedDesc = TableSorting.tableSortingIntDesc(tableOryginal);
+            int[] tableSortedAsc = TableSorting.tableSortingIntAsc(tableOryginal);
 
-        System.out.printf("Całość załeła %s milisekund\n", (nanoTime() - time0) / 1_000);
+            System.out.printf("Całość zajeła %s milisekund\n", (nanoTime() - time0) / 1_000);
 
-        TablePrinter.tablePrinterINT(tableOryginal);
-        TablePrinter.tablePrinterINT(tableSortedDesc);
-        TablePrinter.tablePrinterINT(tableSortedAsc);
-
+            TablePrinter.tablePrinterINT(tableOryginal);
+            TablePrinter.tablePrinterINT(tableSortedDesc);
+            TablePrinter.tablePrinterINT(tableSortedAsc);
+        }
     }
 }
